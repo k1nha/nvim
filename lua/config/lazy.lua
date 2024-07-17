de26cb23df2bb0322db8273cd7fe6e09ac60d228
k1nha <lucascmpus@gmail.com>
2024-07-17 11:47:51 -0300

    add plugin eslint recipies

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -3,7 +3,8 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   -- bootstrap lazy.nvim
   -- stylua: ignore
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
+    lazypath })
 end
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
@@ -20,7 +21,6 @@ require("lazy").setup({
     -- ESLINT --
     -- { import = "lazyvim.plugins.extras.lsp.none-ls" },
 
-    { import = "lazyvim.plugins.extras.linting.eslint " },
     { import = "plugins" },
   },
   defaults = {
